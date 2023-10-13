9e6cc22326fc01e66c44d67991d2c9e599d5b8bc
mrkrook2012 <147385208+mrkrook2012@users.noreply.github.com>
2023-10-13 19:30:48 +0200

    muziek knop toegevoegt
    werkt mogelijk niet op andere computers

---
+++ Legpuzzel_ver1_Meindert/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Legpuzzel_ver1_Meindert
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        bool Moff = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MuziekKnop_Click(object sender, RoutedEventArgs e)
+        {
+            if (Moff == false) { mediaElement.Play(); Moff = true; }
+            else { mediaElement.Stop(); Moff = false; }
+        }
+    }
+}
